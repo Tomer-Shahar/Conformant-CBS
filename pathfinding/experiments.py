888f924fe8f673e19236425a9480acbb1395babf
tomash22123@gmail.com <halflife1>
2019-02-21 17:57:15 +0200

    Added windows condition

--- pathfinding/experiments.py
+++ pathfinding/experiments.py
@@ -216,8 +216,8 @@ if os.name == 'nt':
     exp = Experiments('.\\..\\experiments')
 else:
     exp = Experiments('./../experiments')
-for uncertainty_val in range(0, 5, 1):
-    for num_of_agents in range(10, 30, 5):
+for uncertainty_val in range(2, 5, 1):
+    for num_of_agents in range(15, 30, 5):
         exp.run_experiments_on_same_instance(num_of_agents=num_of_agents, uncertainty=uncertainty_val, time_limit=60, rep_num=30)
 
 print("Finished Experiments")
